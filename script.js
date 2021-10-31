2583d08109b2e05443e38fdcf3bd3c2716a6babc
Oleh Rozhko <oleh.rozhko5@gmail.com>
2021-10-31 18:13:55 +0200

    lesson10 is ready

--- script.js
+++ script.js
@@ -1,75 +1,23 @@
 "use strict";
 
-function reload() {
-  var d = new Date();
-  var currDate = d.getDate();
-  var currMonth = d.getMonth() + 1; //Months are zero based
-  var currYear = d.getFullYear();
-  var currHour = d.getHours();
-  var currMin = d.getMinutes();
-  var currSec = d.getSeconds();
-  var options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    weekday: "long",
-    timezone: "UTC",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-  };
-  var addZero = (number) => {
-    if (number < 10) {
-      return (number = "0" + number);
-    } else {
-      return number;
-    }
-  };
+function inputText() {
+  var input = document.querySelector("input");
+  var list = document.querySelector("ul");
+  list.setAttribute("id", "ul");
+  input.setAttribute("id", "textInput");
 
-  var time =
-    addZero(currDate) +
-    "." +
-    addZero(currMonth) +
-    "." +
-    addZero(currYear) +
-    " - " +
-    addZero(currHour) +
-    ":" +
-    addZero(currMin) +
-    ":" +
-    addZero(currSec);
+  var liLast = document.createElement("li");
 
-  var date = d.toLocaleString("uk", options);
-  var firstPart = "Сьогодні " + date.slice(0, 24) + " року,";
-  var lastPart =
-    addZero(currHour) +
-    " година " +
-    addZero(currMin) +
-    " хвилин " +
-    addZero(currSec) +
-    " секунд";
-  var textTime = firstPart.concat(lastPart);
-
-  document.querySelector(".text-time").textContent = textTime;
-  document.querySelector(".time").textContent = time;
-  console.clear();
-  console.log(textTime);
-  console.log(time);
+  if (input.value != !String) {
+    liLast.innerHTML = input.value;
+    return ul.append(liLast);
+  }
 }
 
-var deleteElem = function () {
-  $("body").remove();
-};
+function clearText() {
+  document.getElementById("textInput").value = "";
+}
 
-let elem = document.createElement("div");
-elem.classList.add("text-time");
-document.body.appendChild(elem);
-
-elem = document.createElement("div");
-elem.classList.add("time");
-document.body.appendChild(elem);
-console.dir(elem);
-
-document.body.style.color = "red";
-
-setInterval(reload, 1000);
+let button = document.querySelector("button");
+button.addEventListener("click", inputText);
+button.addEventListener("click", clearText);
